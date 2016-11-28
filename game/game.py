0b3df69ae57daa0c27cf71a7d5130d72b663958c
Larry Zhang <larryzhang101@gmail.com>
2016-11-28 17:58:33 -0500

    Adding functionality and cleaning up results

--- game/game.py
+++ game/game.py
@@ -28,8 +28,8 @@ class Game(object):
         return self.game.__str__()
 
     def threatened(self):
-        white = [[self.game.attackers(True, row * 8 + col) for col in range(8)] for row in range(8)]
-        black = [[self.game.attackers(False, row * 8 + col) for col in range(8)] for row in range(8)]
+        white = [[map(self.game.piece_at, list(self.game.attackers(True, row * 8 + col))) for col in range(8)] for row in range(8)]
+        black = [[map(self.game.piece_at, list(self.game.attackers(False, row * 8 + col))) for col in range(8)] for row in range(8)]
         return (white, black)
 
     def available(self):
@@ -54,3 +54,8 @@ class Game(object):
         self.game.turn = not self.game.turn
 
         return (legalsquares, otherlegalsquares) if self.game.turn else (otherlegalsquares, legalsquares)
+
+    # True = white, False = black
+    # 1 = pawn, 2 = knight, 3 = bishop, 4 = rook, 5 = queen, 6 = king
+    def pieces(self, color=True, piece_type=1):
+        return map(lambda index: (index / 8, index % 8), list(self.game.pieces(piece_type, color)))
--- test.py
+++ test.py
@@ -1,13 +1,6 @@
 from game.game import *
-import chess
-import os, sys
 
-tests = os.listdir('tests/')
-for test in tests:
-    moves = [line.strip() for line in open('tests/{0}'.format(test)).readlines()]
-    game = chess.Board()
-    for move in moves:
-        try:
-            game.push_san(move)
-        except ValueError as e:
-            print e
+newgame = Game()
+print newgame.available()
+print newgame.threatened()
+print newgame.pieces(True, 1)
