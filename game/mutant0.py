117fe0b58d337b79018b84c3d1a0a9f28f1f7c9b
Ben <benjaminzheng@college.harvard.edu>
2016-12-12 16:35:31 -0500

    merging

--- game/mutant0.py
+++ game/mutant0.py
@@ -2,11 +2,10 @@ import chess, game
 import os, sys
 import eval
 g = game.Game(config='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
-e = eval.ChessAI(g, params=[0.025430824182692987, 0.028407460981331067, 0.012570971348237129, 0.012588494512726823, 0.005948991527664038, 0.035004389456069575, 0.005480314496200913, 0.017485269544117495])
+e = eval.ChessAI(g, params=[0.024562342593483125, 0.027540094386543985, 0.0131791247140367, 0.012289693586295976, 0.006638471293781575, 0.03291191639520942, 0.005010119068196424, 0.018153570581693514])
 moves = 0
 while moves < 10 and not g.over() and not g.isDraw():
 	moves += 1
 	score, move = e.nextMove()
 	g.move(move)
-	print g
 print e.eval()
