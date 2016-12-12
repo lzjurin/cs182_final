d4d0ee58b0dd34fcd45b53019ddf7126b53a65c6
Ben <benjaminzheng@college.harvard.edu>
2016-12-12 16:04:24 -0500

    Odyssey changes

--- game/endgame1a.py
+++ game/endgame1a.py
@@ -10,7 +10,7 @@ g = game.Game(config='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
 curParams = [0.025,0.25,0.0125,0.0125,0.0067,0.033,0.005,0.02]
 moves = 0
 
-while moves < 10 or not g.over() or not g.isDraw():
+while moves < 10 and not g.over() and not g.isDraw():
 	moves += 1
 	allMuts = []
 	for i in range(5):
@@ -22,45 +22,47 @@ while moves < 10 or not g.over() or not g.isDraw():
 					value = curParams[i] * np.random.uniform(1 - np.random.random()*2 / float(moves), 1 + np.random.random()*2 / float(moves))
 			mutParams.append()
 		allMuts.append(mutParams)
-		f1 = open('mutant' + str(i) + '.py', 'w')
-		f1.write('import chess, game')
-		f1.write('import os, sys')
-		f1.write('import eval')
-		f1.write('g = game.Game(config=\'' + g.game.fen() + '\')')
-		f1.write('e = eval.ChessAI(g, params=' + str(mutParams) + ')')
-		f1.write('for _ in range(10):')
-		f1.write('\tscore, move = e.nextMove()')
-		f1.write('\tg.move(move)')
+		f1 = open('mutant' + str(i) + 'a.py', 'w')
+		f1.write('import chess, game\n')
+		f1.write('import os, sys\n')
+		f1.write('import eval\n')
+		f1.write('g = game.Game(config=\'' + g.game.fen() + '\')\n')
+		f1.write('e = eval.ChessAI(g, params=' + str(mutParams) + ')\n')
+		f1.write('moves = 0\n')
+		f1.write('while moves < 10 and not g.over() and not g.isDraw():\n')
+		f1.write('\t moves += 1\n')
+		f1.write('\tscore, move = e.nextMove()\n')
+		f1.write('\tg.move(move)\n')
 		f1.write('print e.eval()')
-		f2 = open('mutant' + str(i) + '.sh', 'w')
-		f2.write('#!/bin/bash')
-		f2.write('#SBATCH -t 60')
-		f2.write('#SBATCH -p serial_requeue')
-		f2.write('#SBATCH -N 1')
-		f2.write('#SBATCH -c 4')
-		f2.write('#SBATCH --mem=32000')
-		f2.write('#SBATCH -o mutant' + str(i) + '.out')
-		f2.write('#SBATCH -e mutant' + str(i) + '.err')
-		f2.write('#SBATCH --mail-type=END')
-		f2.write('#SBATCH --mail-user=benjaminzheng@college.harvard.edu')
-		f2.write('python mutant' + str(i) + '.py')
+		f2 = open('mutant' + str(i) + 'a.sh', 'w')
+		f2.write('#!/bin/bash\n')
+		f2.write('#SBATCH -t 15\n')
+		f2.write('#SBATCH -p serial_requeue\n')
+		f2.write('#SBATCH -N 1\n')
+		f2.write('#SBATCH -c 4\n')
+		f2.write('#SBATCH --mem=16000\n')
+		f2.write('#SBATCH -o mutant' + str(i) + 'a.out\n')
+		f2.write('#SBATCH -e mutant' + str(i) + 'a.err\n')
+		f2.write('#SBATCH --mail-type=END\n')
+		f2.write('#SBATCH --mail-user=benjaminzheng@college.harvard.edu\n')
+		f2.write('python mutant' + str(i) + 'a.py')
 
 		f1.close()
 		f2.close()
 
-		call(["sbatch", "mutant" + str(i) + ".sh"])
+		call(["sbatch", "mutant" + str(i) + "a.sh"])
 
-	best = 99999999999.0
+	best = -99999999999.0
 	for i in range(5):
 		done = False
 		while not done:
-			if os.path.exists('mutant' + str(i) + '.out') and os.path.getsize('mutant' + str(i) + '.out') > 0:
+			if os.path.exists('mutant' + str(i) + 'a.out') and os.path.getsize('mutant' + str(i) + 'a.out') > 0:
 				done = True
-		f3 = open('mutant' + str(i) + '.out', 'r')
+		f3 = open('mutant' + str(i) + 'a.out', 'r')
 		# Parse and find best score
-		result = float(f3.readline())
+		result = float(f3.readlines()[-1])
 		f3.close()
-		if result < best:
+		if result >best:
 			best = result
 			curParams = allMuts[i]
 
--- game/endgame1p.py
+++ game/endgame1p.py
@@ -2,7 +2,6 @@ import chess, game
 import os, sys
 import eval
 from subprocess import call
-import copy
 import numpy as np
 
 # Initialize initial game with passive strategy
@@ -10,7 +9,7 @@ g = game.Game(config='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
 curParams = [0.025,0.025,0.0125,0.0125,0.0067,0.033,0.005,0.02]
 moves = 0
 
-while moves < 10 or not g.over() or not g.isDraw():
+while moves < 10 and not g.over() and not g.isDraw():
 	moves += 1
 	allMuts = []
 	for i in range(5):
@@ -19,26 +18,28 @@ while moves < 10 or not g.over() or not g.isDraw():
 			mutParams.append(el * np.random.uniform(1 - np.random.random()*2 / float(moves), 1 + np.random.random()*2 / float(moves)))
 		allMuts.append(mutParams)
 		f1 = open('mutant' + str(i) + '.py', 'w')
-		f1.write('import chess, game')
-		f1.write('import os, sys')
-		f1.write('import eval')
-		f1.write('g = game.Game(config=\'' + g.game.fen() + '\')')
-		f1.write('e = eval.ChessAI(g, params=' + str(mutParams) + ')')
-		f1.write('for _ in range(10):')
-		f1.write('\tscore, move = e.nextMove()')
-		f1.write('\tg.move(move)')
+		f1.write('import chess, game\n')
+		f1.write('import os, sys\n')
+		f1.write('import eval\n')
+		f1.write('g = game.Game(config=\'' + g.game.fen() + '\')\n')
+		f1.write('e = eval.ChessAI(g, params=' + str(mutParams) + ')\n')
+		f1.write('moves = 0\n')
+		f1.write('while moves < 10 and not g.over() and not g.isDraw():\n')
+		f1.write('\t moves += 1\n')
+		f1.write('\tscore, move = e.nextMove()\n')
+		f1.write('\tg.move(move)\n')
 		f1.write('print e.eval()')
 		f2 = open('mutant' + str(i) + '.sh', 'w')
-		f2.write('#!/bin/bash')
-		f2.write('#SBATCH -t 60')
-		f2.write('#SBATCH -p serial_requeue')
-		f2.write('#SBATCH -N 1')
-		f2.write('#SBATCH -c 4')
-		f2.write('#SBATCH --mem=32000')
-		f2.write('#SBATCH -o mutant' + str(i) + '.out')
-		f2.write('#SBATCH -e mutant' + str(i) + '.err')
-		f2.write('#SBATCH --mail-type=END')
-		f2.write('#SBATCH --mail-user=benjaminzheng@college.harvard.edu')
+		f2.write('#!/bin/bash\n')
+		f2.write('#SBATCH -t 15\n')
+		f2.write('#SBATCH -p serial_requeue\n')
+		f2.write('#SBATCH -N 1\n')
+		f2.write('#SBATCH -c 4\n')
+		f2.write('#SBATCH --mem=16000\n')
+		f2.write('#SBATCH -o mutant' + str(i) + '.out\n')
+		f2.write('#SBATCH -e mutant' + str(i) + '.err\n')
+		f2.write('#SBATCH --mail-type=END\n')
+		f2.write('#SBATCH --mail-user=benjaminzheng@college.harvard.edu\n')
 		f2.write('python mutant' + str(i) + '.py')
 
 		f1.close()
@@ -46,7 +47,7 @@ while moves < 10 or not g.over() or not g.isDraw():
 
 		call(["sbatch", "mutant" + str(i) + ".sh"])
 
-	best = 99999999999.0
+	best = -99999999999.0
 	for i in range(5):
 		done = False
 		while not done:
@@ -54,9 +55,9 @@ while moves < 10 or not g.over() or not g.isDraw():
 				done = True
 		f3 = open('mutant' + str(i) + '.out', 'r')
 		# Parse and find best score
-		result = float(f3.readline())
+		result = float(f3.readlines()[-1])
 		f3.close()
-		if result < best:
+		if result > best:
 			best = result
 			curParams = allMuts[i]
 
