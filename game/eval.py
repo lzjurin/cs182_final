6fbb3d97bc60dc4b9fefcef317de44a7aab0c03c
shaynpiece <shaynlozano@college.harvard.edu>
2016-12-12 08:00:49 -0500

    compiling eval function

--- game/eval.py
+++ game/eval.py
@@ -2,19 +2,19 @@ import chess, game
 import os, sys
 
 class ChessAI:
-    def __init__(self, gamestate, params=[1, 1]):
+    def __init__(self, gamestate, params=[1,1,1,1,1,1,1]):
         self.gamestate = gamestate
         self.params = params
         self.materialWeight = 1
         self.threatWeight = 1
-        self.kingSafetyTotalAttackers={1:0,2:50,3:75,4:88,5:94,6:97,7:99,8:99,9:99,10:99}
+        self.kingSafetyTotalAttackers={0:0,1:0,2:50,3:75,4:88,5:94,6:97,7:99,8:99,9:99,10:99}
         self.kingSafetyThreats={chess.Piece.from_symbol('P'):1,chess.Piece.from_symbol('B'): 20,chess.Piece.from_symbol('N'): 20,chess.Piece.from_symbol('R'): 40, chess.Piece.from_symbol('Q'): 80}
         self.pawnPositionBonus =  [[0, 0, 0, 0, 0, 0, 0, 0], [50, 50, 50, 50, 50, 50, 50, 50], [10, 10, 20, 30, 30, 20, 10, 10], [5, 5, 10, 25, 25, 10, 5, 5], [0, 0, 0, 20, 20, 0, 0, 0], [5, -5, -10, 0, 0, -10, -5, 5], [5, 10, 10, -20, -20, 10, 10, 5], [0, 0, 0, 0, 0, 0, 0, 0]]
         self.knightPositionBonus = [[-50, -40, -30, -30, -30, -30, -40, -50], [-40, -20, 0, 0, 0, 0, -20, -40], [-30, 0, 10, 15, 15, 10, 0, -30], [-30, 5, 15, 20, 20, 15, 5, -30], [-30, 0, 15, 20, 20, 15, 0, -30], [-30, 5, 10, 15, 15, 10, 5, -30], [-40, -20, 0, 5, 5, 0, -20, -40], [-50, -40, -30, -30, -30, -30, -40, -50]]
         self.bishopPositionBonus = [[-20, -10, -10, -10, -10, -10, -10, -20], [-10, 0, 0, 0, 0, 0, 0, -10], [-10, 0, 5, 10, 10, 5, 0, -10], [-10, 5, 5, 10, 10, 5, 5, -10], [-10, 0, 10, 10, 10, 10, 0, -10], [-10, 10, 10, 10, 10, 10, 10, -10], [-10, 5, 0, 0, 0, 0, 5, -10], [-20, -10, -10, -10, -10, -10, -10, -20]]
         self.rookPositionBonus = [[0, 0, 0, 0, 0, 0, 0, 0], [5, 10, 10, 10, 10, 10, 10, 5], [-5, 0, 0, 0, 0, 0, 0, -5], [-5, 0, 0, 0, 0, 0, 0, -5], [-5, 0, 0, 0, 0, 0, 0, -5], [-5, 0, 0, 0, 0, 0, 0, -5], [-5, 0, 0, 0, 0, 0, 0, -5], [0, 0, 0, 5, 5, 0, 0, 0]]
         self.queenPositionBonus = [[-20, -10, -10, -5, -5, -10, -10, -20], [-10, 0, 0, 0, 0, 0, 0, -10], [-10, 0, 5, 5, 5, 5, 0, -10], [-5, 0, 5, 5, 5, 5, 0, -5], [0, 0, 5, 5, 5, 5, 0, -5], [-10, 5, 5, 5, 5, 5, 0, -10], [-10, 0, 5, 0, 0, 0, 0, -10], [-20, -10, -10, -5, -5, -10, -10, -20]]
-        self.kingEarlyPositionBonus[[-30, -40, -40, -50, -50, -40, -40, -30], [-30, -40, -40, -50, -50, -40, -40, -30], [-30, -40, -40, -50, -50, -40, -40, -30], [-30, -40, -40, -50, -50, -40, -40, -30], [-20, -30, -30, -40, -40, -30, -30, -20], [-10, -20, -20, -20, -20, -20, -20, -10], [20, 20, 0, 0, 0, 0, 20, 20], [20, 30, 10, 0, 0, 10, 30, 20]]
+        self.kingEarlyPositionBonus = [[-30, -40, -40, -50, -50, -40, -40, -30], [-30, -40, -40, -50, -50, -40, -40, -30], [-30, -40, -40, -50, -50, -40, -40, -30], [-30, -40, -40, -50, -50, -40, -40, -30], [-20, -30, -30, -40, -40, -30, -30, -20], [-10, -20, -20, -20, -20, -20, -20, -10], [20, 20, 0, 0, 0, 0, 20, 20], [20, 30, 10, 0, 0, 10, 30, 20]]
         self.kingLatePositionBonus = [[-50, -40, -30, -20, -20, -30, -40, -50], [-30, -20, -10, 0, 0, -10, -20, -30], [-30, -10, 20, 30, 30, 20, -10, -30], [-30, -10, 30, 40, 40, 30, -10, -30], [-30, -10, 30, 40, 40, 30, -10, -30], [-30, -10, 20, 30, 30, 20, -10, -30], [-30, -30, 0, 0, 0, 0, -30, -30], [-50, -30, -30, -30, -30, -30, -30, -50]]
         self.index = {1: self.pawnPositionBonus, 2: self.knightPositionBonus, 3: self.bishopPositionBonus, 4: self.rookPositionBonus, 5: self.queenPositionBonus, 6: self.kingEarlyPositionBonus, 7: self.kingEarlyPositionBonus}
 
@@ -28,37 +28,41 @@ class ChessAI:
         if side == 1:
             return whiteScore
 
-    def advance(self):
-        wSum = -self.gamestate.pieces(True,6)[0][0]
-        bSum = 8 - self.gamestate.pieces(False,6)[0][0]
-        for n in range(1,6):
-            for piece in self.gamestate.pieces(True,n):
-                wS += min(1,piece[0]/float(4)) if
-            for piece in self.gamestate.Bpieces:
-                bSum += int(piece[-1])
-        for k in self.gamestate.Wknights:
-            if '1' in k or '8' in k or 'a' in k or 'h' in k:
-                wSum -= 1
-        for k in self.gamestate.Bknights:
-            if '1' in k or '8' in k or 'a' in k or 'h' in k:
-                bSum -= 1
-        for b in self.gamestate.Wbishops:
-            if b == 'a1' or b == 'a8' or b=='h1' or b=='h8':
-                wSum -= 1
-        for b in self.gamestate.Bknights:
-            if b == 'a1' or b == 'a8' or b=='h1' or b=='h8':
-                bSum -= 1
-        return wSum - bSum
+    # def advance(self):
+    #     wSum = 0
+    #     bSum = 0
+    #     for n in range(1,6):
+    #         for piece in self.gamestate.pieces(True,n):
+    #             wSum += min(1,piece[0]/float(4))
+    #         for piece in self.gamestate.Bpieces:
+    #             bSum += int(piece[-1])
+    #     # for k in self.gamestate.Wknights:
+    #     #     if '1' in k or '8' in k or 'a' in k or 'h' in k:
+    #     #         wSum -= 1
+    #     # for k in self.gamestate.Bknights:
+    #     #     if '1' in k or '8' in k or 'a' in k or 'h' in k:
+    #     #         bSum -= 1
+    #     # for b in self.gamestate.Wbishops:
+    #     #     if b == 'a1' or b == 'a8' or b=='h1' or b=='h8':
+    #     #         wSum -= 1
+    #     # for b in self.gamestate.Bknights:
+    #     #     if b == 'a1' or b == 'a8' or b=='h1' or b=='h8':
+    #     #         bSum -= 1
+    #     return wSum - bSum
     # def pawns(self):
     def space(self):
-        wTotal = 0
-        bTotal = 0
-        for k in self.gamestate.available().keys():
-            if k[0]<4:
-                wTotal += 1
-            else:
-                bTotal += 1
-        return wTotal - bTotal
+        if self.gamestate.turn == "White":
+            return len(self.gamestate.available()[0]) - len(self.gamestate.available()[1])
+        else:
+            return len(self.gamestate.available()[1]) - len(self.gamestate.available()[0])
+        # wTotal = 0
+        # bTotal = 0
+        # for k in self.gamestate.available().keys():
+        #     if k[0]<4:
+        #         wTotal += 1
+        #     else:
+        #         bTotal += 1
+        # return wTotal - bTotal
     def threat(self):
         wSum = 0
         bSum = 0
@@ -113,7 +117,7 @@ class ChessAI:
         return (val, self.gamestate.legalMoves()[vals.index(val)])
 
     def eval(self):
-        return sum(map(lambda (x, y): float(x) * y, zip(self.params, [self.threat(), self.material()])))
+        return sum(map(lambda (x, y): float(x) * y, zip(self.params, [self.threat(), self.material(), self.space(),self.pieceSpecific(),self.pieceValues(),self.pawnStructure(), self.kingSafety()])))
 
     def moveEval(self):
         moves = []
@@ -122,18 +126,25 @@ class ChessAI:
             moves.append(c.eval(),move)
         return max(moves)[1]
     def pieceValues(self):
-        def helper(self,isWhite):
+        def helper(isWhite):
             total = 0
             for n in range (1,7):
                 pieces = self.gamestate.pieces(isWhite,n)
                 for (y,x) in pieces:
-                    if n == 6 and self.material(int(not isWhite)) < 13:
-                        total += self.index[n+1][x][7]
-                    else
-                        total += self.index[n][x][y]
+                    if isWhite:
+                        if n == 6 and self.material(int(not isWhite)) < 13:
+                            total += self.index[n+1][x][7]
+                        else:
+                            total += self.index[n][x][y]
+                    else:
+                        if n == 6 and self.material(int(not isWhite)) < 13:
+                            total += list(reversed(self.index[n+1]))[x][7]
+                        else:
+                            total += list(reversed(self.index[n]))[x][y]
+            return total
         return helper(True) - helper(False)
     def pieceSpecific(self):
-        def helper(self,isWhite):
+        def helper(isWhite):
             total = 0
             pawns = self.gamestate.pieces(isWhite,1)
             knights = self.gamestate.pieces(isWhite,2)
@@ -142,9 +153,9 @@ class ChessAI:
             queen = self.gamestate.pieces(isWhite,5)
             for (y,x) in pawns:
                 # Enemy king Y, Enemy king X
-                eKY,eKX = self.gamestate.pieces(not isWhite,6)
+                eKY,eKX = self.gamestate.pieces(not isWhite,6)[0]
                 if abs(eKX - x) <= 1:
-                    total += 1.0/abs(eKY - Y)
+                    total += 1.0/abs(eKY - y)
             if isWhite:
                 for (y,x) in knights:
                     if chess.Piece.from_symbol('P') in self.gamestate.threatened()[int(not isWhite)][y][x] and not [(py,px) for (py,px) in self.gamestate.pieces(isWhite,1) if abs(px-x) <= 1 and px != x and py > y]:
@@ -175,11 +186,10 @@ class ChessAI:
                     pawnsInFile = filter(lambda (py,px): x == px, enemyPawns + pawns)
                     if pawnsInFile == 0:
                         total += 2
+            return total
         return helper(True) - helper(False)
-
-
     def pawnStructure(self):
-        def helper(self,isWhite):
+        def helper(isWhite):
             total = 0
             pawns = self.gamestate.pieces(isWhite,1)
             differentXValues=[]
@@ -207,15 +217,17 @@ class ChessAI:
             else:
                 if len([(y,x) for (y,x) in pawns if y == min(pawns)[0]]) == 1 and chess.Piece.from_symbol('P') in self.gamestate.threatened()[1][y-1][x]:
                     total -= 3
+            return total
         return helper(True) - helper(False)
 
     def kingSafety(self):
         whiteKingPos = self.gamestate.pieces(True,6)[0]
         blackKingPos = self.gamestate.pieces(False,6)[0]
-        def helper(self, isWhite):
-            y,x = self.gamestate.pieces(isWhite,6)[0]
+        def helper(isWhite):
+            (y,x) = self.gamestate.pieces(isWhite,6)[0]
             total = 0
-            if castled:
+            # CHANGE TO CHECK FOR IF CASTLED INSTEAD
+            if True:
                 total += 100
                 pawns = self.gamestate.pieces(isWhite,1)
                 if isWhite:
@@ -226,19 +238,20 @@ class ChessAI:
                     for p in pawns:
                         if p == (y-1,x) or p == (y-1,x+1) or p == (y-1,x-1):
                             total += 10
-            kingZone = self.gamestate.legalmoves(y,x)
+            # CHANGE TO REPRESENT ACTUAL KINGZONE
+            kingZone = self.gamestate.kingzone(isWhite)
             if self.material(int(not isWhite)) > 12:
                 subtotal = 0
                 totalAttackingPieces = 0
                 attackingPieces = []
-                for y,x in kingzone:
+                for y,x in kingZone:
                     for threat in self.gamestate.threatened()[int(isWhite)][y][x]:
                         subtotal += self.kingSafetyThreats[threat[1]]
                         if threat[0] in attackingPieces:
                             continue
                         else:
                             attackingPieces.append(threat[0])
-                return total - (kingSafetyTotalAttackers[totalAttackingPieces] * subtotal)/100.0
+                return total - (self.kingSafetyTotalAttackers[totalAttackingPieces] * subtotal)/100.0
             else:
                 return total
         return helper(True) - helper(False)
