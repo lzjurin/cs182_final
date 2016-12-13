88193c34e59a7ed7ac3e08ce1fcd4cd11f36d103
Larry Zhang <larryzhang101@gmail.com>
2016-12-12 23:28:07 -0500

    Documentation and testing

--- game/game.py
+++ game/game.py
@@ -4,11 +4,17 @@ from collections import deque
 
 class Game(object):
     def __init__(self, config=None):
+
+        # If a config is specified, set it up that way, otherwise start out default
         if not config:
             self.game = chess.Board()
         else:
             self.game = chess.Board(fen=config)
+
+        # Get a list of squares
         self.squares = chess.SQUARES
+
+        # Store dictionary of pieces of lesser value
         self.WLesserValue = {1: [],2:[chess.Piece.from_symbol('p')],\
         3:[chess.Piece.from_symbol('p')],4:[chess.Piece.from_symbol('p'),chess.Piece.from_symbol('b'),chess.Piece.from_symbol('n')],\
         5: [chess.Piece.from_symbol('p'),chess.Piece.from_symbol('b'),chess.Piece.from_symbol('n'),chess.Piece.from_symbol('r')],\
@@ -17,20 +23,35 @@ class Game(object):
         3:[chess.Piece.from_symbol('P')],4:[chess.Piece.from_symbol('P'),chess.Piece.from_symbol('B'),chess.Piece.from_symbol('N')],\
         5: [chess.Piece.from_symbol('P'),chess.Piece.from_symbol('B'),chess.Piece.from_symbol('N'),chess.Piece.from_symbol('R')],\
         6: [chess.Piece.from_symbol('P'),chess.Piece.from_symbol('B'),chess.Piece.from_symbol('N'),chess.Piece.from_symbol('R'),chess.Piece.from_symbol('Q')]}
+
+        # Store dictionary of piece type to values, and whether a side has castled
         self.values = {1:1,2:3,3:3,4:5,5:9,6:1000}
         self.castled = [False] * 2
 
+    # Check if a given square is pinned by an enemy piece
+    # Only counts as pinned if the enemy piece has lesser value than the pinned piece which in turn must have lesser value than the piece behind it
+    # OR if both the pinned pieces are "free", i.e. not defended
     def pinned(self, square):
+
+        # Find the piece at the square, if None then it can't be pinned
         piece = self.game.piece_at(square)
         if not piece:
             return False
+
+        # Get all attackers and find each attacker's attacked squares
         attackers = list(self.game.attackers(not piece.color, square))
         attackedsquares = map(self.game.attacks, attackers)
+
+        # Store old movestack for resetting when done checking pinning
         movestack = deque(map(lambda move: chess.Move.from_uci(move.uci()), list(self.game.move_stack)))
+
+        # Remove the piece and get new attacked squares to look for hidden attacks, then put back the piece and movestack
         self.game.remove_piece_at(square)
         newsquares = map(self.game.attacks, attackers)
         self.game.set_piece_at(square, piece)
         self.game.move_stack = movestack
+
+        # Iterate over each attacker and look for qualifying pins
         for i in xrange(len(attackers)):
             attacker = self.game.piece_at(attackers[i])
             for pos in newsquares[i]:
@@ -38,15 +59,23 @@ class Game(object):
                     return True
         return False
 
+    # Check if a square is defended by a given player
     def defended(self, square, color=True):
         return any(self.game.attackers(color, square))
 
+    # Check if a given square is being forked by an enemy piece
     def forked(self, square):
+
+        # Find the piece at the square, if None then it can't be forked
         piece = self.game.piece_at(square)
         if not piece:
             return False
+
+        # Get all attackers and find each attacker's attacked squares
         attackers = list(self.game.attackers(not piece.color, square))
         attackedsquares = map(self.game.attacks, attackers)
+
+        # Iterate over attackers and look for qualifying forks
         for i in xrange(len(attackers)):
             attacker = self.game.piece_at(attackers[i])
             for attacked in attackedsquares[i]:
@@ -54,6 +83,7 @@ class Game(object):
                     return True
         return False
 
+    # Make a move and record castling moves if one is made
     def move(self, move):
         try:
             if self.game.is_castling(move):
@@ -62,18 +92,22 @@ class Game(object):
         except Exception as e:
             print e
 
+    # Make a move passed in SAN format
+    # DO NOT USE, NOT UPKEPT
     def move_san(self, move):
         try:
             self.game.push_san(move)
         except ValueError as e:
             print e
 
+    # Undo previous move
     def undo(self):
         try:
             self.game.pop()
         except Exception as e:
             print e
 
+    # Get a list of legal moves, with optional constraints of a starting square and a player
     def legalMoves(self, start=None, color=None):
         if not start:
             if not color:
@@ -87,9 +121,11 @@ class Game(object):
         else:
             return [move for move in self.game.legal_moves if move.from_square == (start[0] * 8 + start[1])]
 
+    # Check whether a player has castled (defaults to White)
     def hasCastled(self, color=True):
         return self.castled[int(color)]
 
+    # Gets a list of all the squares that comprise the kingzone given the current king position
     def kingzone(self, color=True):
         position = list(self.game.pieces(6, color))[0]
         coords = [position / 8, position % 8]
@@ -97,29 +133,34 @@ class Game(object):
         possibles = [pos for pos in possibles if (pos[0] * 8 + pos[1]) > -1 and (pos[0] * 8 + pos[1]) < 64]
         return possibles
 
+    # Get whose turn it is (both string and boolean formats)
     def turn(self):
         return "White" if self.game.turn else "Black"
-
     def turn_bool(self):
         return self.game.turn
 
+    # Get a list of all the current positions of pieces on the board
     def getPositions(self):
         return [row.split() for row in self.game.__str__().split('\n')]
 
+    # Is the game over? Is it a draw?
     def over(self):
         return self.game.is_game_over()
 
     def isDraw(self):
         return self.game.can_claim_threefold_repetition()
 
+    # String representation - propagate up the str representation from the chessboard. This is a wrapper, after all, not a reimplementation. :P
     def __str__(self):
         return self.game.__str__()
 
+    # Get a list of threatened squares
     def threatened(self):
         white = [[map(lambda pos: ((pos / 8, pos % 8), self.game.piece_at(pos)), list(self.game.attackers(True, row * 8 + col))) for col in range(8)] for row in range(8)]
         black = [[map(lambda pos: ((pos / 8, pos % 8), self.game.piece_at(pos)), list(self.game.attackers(False, row * 8 + col))) for col in range(8)] for row in range(8)]
         return (white, black)
 
+    # Get a list of available squares on each side
     def available(self):
         legal = [move for move in self.game.legal_moves]
         legalsquares = {}
