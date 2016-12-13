e96b756b53402d946e21ed40e8e97e457bc0c230
shaynpiece <shaynlozano@college.harvard.edu>
2016-12-12 23:34:53 -0500

    comments

--- game/eval.py
+++ game/eval.py
@@ -20,6 +20,7 @@ class ChessAI:
         self.kingLatePositionBonus = [[-50, -40, -30, -20, -20, -30, -40, -50], [-30, -20, -10, 0, 0, -10, -20, -30], [-30, -10, 20, 30, 30, 20, -10, -30], [-30, -10, 30, 40, 40, 30, -10, -30], [-30, -10, 30, 40, 40, 30, -10, -30], [-30, -10, 20, 30, 30, 20, -10, -30], [-30, -30, 0, 0, 0, 0, -30, -30], [-50, -30, -30, -30, -30, -30, -30, -50]]
         self.index = {1: self.pawnPositionBonus, 2: self.knightPositionBonus, 3: self.bishopPositionBonus, 4: self.rookPositionBonus, 5: self.queenPositionBonus, 6: self.kingEarlyPositionBonus, 7: self.kingEarlyPositionBonus}
 
+    # Returns the difference in the amount of material on each side of the board, with a positive return value showing a white advantage
     def material(self,side=-1):
         whiteScore = len(self.gamestate.pieces(True,1)) + 3 * (len(self.gamestate.pieces(True,2)) + len(self.gamestate.pieces(True,3))) + 5 * len(self.gamestate.pieces(True,4)) + 9 * len(self.gamestate.pieces(True,5))
         blackScore = len(self.gamestate.pieces(False,1)) + 3 * (len(self.gamestate.pieces(False,2)) + len(self.gamestate.pieces(False,3))) + 5 * len(self.gamestate.pieces(False,4)) + 9 * len(self.gamestate.pieces(False,5))
@@ -30,28 +31,7 @@ class ChessAI:
         if side == 1:
             return whiteScore
 
-    # def advance(self):
-    #     wSum = 0
-    #     bSum = 0
-    #     for n in range(1,6):
-    #         for piece in self.gamestate.pieces(True,n):
-    #             wSum += min(1,piece[0]/float(4))
-    #         for piece in self.gamestate.Bpieces:
-    #             bSum += int(piece[-1])
-    #     # for k in self.gamestate.Wknights:
-    #     #     if '1' in k or '8' in k or 'a' in k or 'h' in k:
-    #     #         wSum -= 1
-    #     # for k in self.gamestate.Bknights:
-    #     #     if '1' in k or '8' in k or 'a' in k or 'h' in k:
-    #     #         bSum -= 1
-    #     # for b in self.gamestate.Wbishops:
-    #     #     if b == 'a1' or b == 'a8' or b=='h1' or b=='h8':
-    #     #         wSum -= 1
-    #     # for b in self.gamestate.Bknights:
-    #     #     if b == 'a1' or b == 'a8' or b=='h1' or b=='h8':
-    #     #         bSum -= 1
-    #     return wSum - bSum
-    # def pawns(self):
+    # Returns a yuge bonus for find a checkmate move
     def checkmate(self):
         if self.gamestate.game.is_checkmate():
             if self.gamestate.turn() == "White":
@@ -59,19 +39,15 @@ class ChessAI:
             elif self.gamestate.turn() == "Black":
                 return 5000
         return 0
+
+    # Returns the difference in the amount of available squares on each side of the board, with a white advantage being positive
     def space(self):
         if self.gamestate.turn == "White":
             return len(self.gamestate.available()[0]) - len(self.gamestate.available()[1])
         else:
             return len(self.gamestate.available()[1]) - len(self.gamestate.available()[0])
-        # wTotal = 0
-        # bTotal = 0
-        # for k in self.gamestate.available().keys():
-        #     if k[0]<4:
-        #         wTotal += 1
-        #     else:
-        #         bTotal += 1
-        # return wTotal - bTotal
+
+    # Returns a value reflecting the defensive and offensive state of each team
     def threat(self):
         wSum = 0
         bSum = 0
@@ -90,24 +66,28 @@ class ChessAI:
                         wSum -= 1
                         smallerThreat = True
                     pieceThreat += 1
+                # Awards points for defending pieces
                 for friendlyPiece in self.gamestate.threatened()[0][y][x]:
                     if friendlyPiece[1] in self.gamestate.BLesserValue[n]:
                         wSum += .25
                     pieceThreat -= 1
+                # Determines if piece is in danger of being taken
                 if pieceThreat > 0 and smallerThreat:
                     wSum -= self.gamestate.values[n]
+                # Assesses penalty for all pins
                 if self.gamestate.pinned(y*8 + x):
                     if n == 6:
                         wSum -= 9
                     else:
                         wSum-= self.gamestate.values[n]
+                # Assesses penalty for all forks
                 if self.gamestate.forked(y*8 + x):
                     if n == 6:
                         wSum -= 9
                     else:
                         wSum -= self.gamestate.values[n]
+            # Repeats process for black pieces
             for y,x in bPieces:
-                # Get coordinates of attacking pieces
                 pieceThreat = 0
                 smallerThreat = False
                 for enemyPiece in self.gamestate.threatened()[0][y][x]:
@@ -132,9 +112,11 @@ class ChessAI:
                     else:
                         bSum -= self.gamestate.values[n]
         return wSum - bSum
+    # Returns difference in mobility between white and black
     def mobility(self):
         return len(self.gamestate.legalMoves(color=True)) - len(self.gamestate.legalMoves(color=False))
 
+    # Returns a tuple of the best next move and the value associated with it, in the form (val,move)
     def nextMove(self, depth=1):
         if not depth:
             return (self.eval(), None)
@@ -154,9 +136,11 @@ class ChessAI:
         val = func(vals)
         return (val, self.gamestate.legalMoves()[vals.index(val)])
 
+    # Calls all subevaluation functions, returns result of adding them together and weighting with parameters
     def eval(self):
         return sum(map(lambda (x, y): float(x) * y, zip(self.params, [self.threat(), self.material(), self.space(),self.pieceSpecific(),self.pieceValues(),self.pawnStructure(), self.kingSafety(), self.mobility(),self.checkmate()])))
 
+    # Returns the best move
     def moveEval(self):
         moves = []
         for move in self.gamestate.legalMoves():
@@ -164,13 +148,17 @@ class ChessAI:
             moves.append(c.eval(),move)
         return max(moves)[1]
 
+    # Returns bonus or penalty for each piece depending on where it is on the board, index tables taken from https://chessprogramming.wikispaces.com/Simplified+evaluation+function
     def pieceValues(self):
         def helper(isWhite):
             total = 0
+            # Iterates over all possible classes of pieces
             for n in range (1,7):
+                # Iterates over all pieces for the given color
                 pieces = self.gamestate.pieces(isWhite,n)
                 for (y,x) in pieces:
                     if isWhite:
+                        # Allows for differentiation of end game king versus early and mid game king positioning needs
                         if n == 6 and self.material(int(not isWhite)) < 13:
                             total += self.index[n+1][x][y]
                         else:
@@ -182,26 +170,33 @@ class ChessAI:
                             total += self.index[n][x][7-y]
             return total
         return helper(True) - helper(False)
+
+    # Returns bonuses for certain positions, including pawn outposts and rooks being on open files
     def pieceSpecific(self):
         def helper(isWhite):
+            # Initializes positions
             total = 0
             pawns = self.gamestate.pieces(isWhite,1)
             knights = self.gamestate.pieces(isWhite,2)
             bishops = self.gamestate.pieces(isWhite,3)
             rooks = self.gamestate.pieces(isWhite,4)
             queen = self.gamestate.pieces(isWhite,5)
+            # Gives bonus for pawns storming a castled king
             for (y,x) in pawns:
                 # Enemy king Y, Enemy king X
                 eKY,eKX = self.gamestate.pieces(not isWhite,6)[0]
                 if abs(eKX - x) <= 1:
                     total += 1.0/max(abs(eKY - y),1)
             if isWhite:
+                # Gives bonus for knight being on a pawn outpost
                 for (y,x) in knights:
                     if chess.Piece.from_symbol('P') in self.gamestate.threatened()[int(not isWhite)][y][x] and not [(py,px) for (py,px) in self.gamestate.pieces(isWhite,1) if abs(px-x) <= 1 and px != x and py > y]:
                         total += 5
+                # Gives bonus for bishop being on a pawn outpost
                 for (y,x) in bishops:
                     if chess.Piece.from_symbol('P') in self.gamestate.threatened()[int(not isWhite)][y][x] and not [(py,px) for (py,px) in self.gamestate.pieces(isWhite,1) if abs(px-x) <= 1 and px != x and py > y]:
                         total += 3
+                # Gives bonus for being on open file and for threatening enemy rows of pawns
                 for (y,x) in rooks:
                     enemyPawns = self.gamestate.pieces(not isWhite,1)
                     pawnsInRow = filter(lambda (py,px): y == py, enemyPawns)
@@ -210,6 +205,7 @@ class ChessAI:
                     pawnsInFile = filter(lambda (py,px): x == px, enemyPawns + pawns)
                     if pawnsInFile == 0:
                         total += 2
+            # Does the same for the other color
             else:
                 for (y,x) in knights:
                     if chess.Piece.from_symbol('p') in self.gamestate.threatened()[int(not isWhite)][y][x] and not [(py,px) for (py,px) in self.gamestate.pieces(isWhite,1) if abs(px-x) <= 1 and px != x and py < y]:
@@ -227,33 +223,41 @@ class ChessAI:
                         total += 2
             return total
         return helper(True) - helper(False)
+
+    # Gives bonuses for having supporting and passed pawns, gives penalties for having backwards, isolated, or stacked pawns
     def pawnStructure(self):
         def helper(isWhite):
             total = 0
+            # Gets pawn positions
             pawns = self.gamestate.pieces(isWhite,1)
             differentXValues=[]
             for y,x in pawns:
-                # if not filter(lambda (a,b): b==x, self.gamestate.pieces(not isWhite,1)):
-                #     total += 1
                 if isWhite:
+                    # Awards bonus for supporting pawns
                     for piece in self.gamestate.threatened()[0][y][x]:
                         if piece == chess.Piece.from_symbol('P'):
                             total += 1
                 else:
+                    # Awards bonus for supporting pawns to black team
                     for piece in self.gamestate.threatened()[1][y][x]:
                         if piece == chess.Piece.from_symbol('p'):
                             total += 1
+                # Awards bonus for passed pawns
                 if not filter(lambda (a,b): b==x or b==x-1 or b==x+1, self.gamestate.pieces(not isWhite,1)):
                     total += 3
+                # Assesses penalties for isolated pawns
                 if not filter(lambda (a,b): b==x-1 or b==x+1, self.gamestate.pieces(isWhite,1)):
                     total -= 2
                 if x not in differentXValues:
                     differentXValues.append(x)
+            # Assesses penalties for stacked pawns
             total -= (len(pawns) - len(differentXValues))
             if isWhite:
+                # Assesses penalty for backwards pawns
                 if len([(y,x) for (y,x) in pawns if y == min(pawns)[0]]) == 1 and chess.Piece.from_symbol('p') in self.gamestate.threatened()[1][y+1][x]:
                     total -= 3
             else:
+                # Assesses penalty for backwards pawns for black
                 if len([(y,x) for (y,x) in pawns if y == min(pawns)[0]]) == 1 and chess.Piece.from_symbol('P') in self.gamestate.threatened()[1][y-1][x]:
                     total -= 3
             return total
@@ -265,22 +269,27 @@ class ChessAI:
         def helper(isWhite):
             (y,x) = self.gamestate.pieces(isWhite,6)[0]
             total = 0
+            # Awards bonus for castling
             if self.gamestate.hasCastled(color=isWhite):
                 total += 100
                 pawns = self.gamestate.pieces(isWhite,1)
                 if isWhite:
+                    # Awards bonus for protecting king with pawns in whichever direction the king castled
                     for p in pawns:
                         if p == (y+1,x) or p == (y+1,x+1) or p == (y+1,x-1):
                             total += 10
                 else:
+                    # Does the same for black
                     for p in pawns:
                         if p == (y-1,x) or p == (y-1,x+1) or p == (y-1,x-1):
                             total += 10
+            # Initializes king zone
             kingZone = self.gamestate.kingzone(isWhite)
             if self.material(int(not isWhite)) > 12:
                 subtotal = 0
                 totalAttackingPieces = 0
                 attackingPieces = []
+                # Counts number of attacking pieces and rank of attacking pieces, assesses penalty using formula given on website
                 for y,x in kingZone:
                     for threat in self.gamestate.threatened()[int(isWhite)][y][x]:
                         if isWhite:
@@ -296,6 +305,7 @@ class ChessAI:
                             else:
                                 attackingPieces.append(threat[0])
                 return total - (self.kingSafetyTotalAttackers[totalAttackingPieces] * subtotal)/100.0
+            # Disables king zone for end game, since king needs to be more aggressive
             else:
                 return total
         return helper(True) - helper(False)
